c5507e452039c0fae047f6e2c1ce66d5cac38684
DAud_IcI <daud.ici@gmail.com>
2019-12-08 20:12:42 +0100

    Initial commit.

---
+++ JsonEditor/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JsonEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ViewModel ViewModel { get; set; } = new ViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel = new ViewModel();
+            InvalidateVisual();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "JSON files|*.json|All files|*.*";
+            if (dialog.ShowDialog() != true) return;
+
+            using var reader = File.OpenText(dialog.FileName);
+            using var jsonReader = new JsonTextReader(reader);
+            ViewModel.Json = JObject.Load(jsonReader);
+            ViewModel.JsonString = ViewModel.Json.ToString();
+        }
+
+        private void Save(bool compact)
+        {
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e) => Save(false);
+
+        private void SaveCompact_Click(object sender, RoutedEventArgs e) => Save(true);
+
+        private void Exit_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!ViewModel.IsChanged()) return;
+            var confirm = MessageBox.Show("Are you sure you want to exit? Your unsaved changes will be lost.",
+                "Confirm Exit", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (confirm != MessageBoxResult.Yes)
+                e.Cancel = true;
+        }
+    }
+}
---
+++ JsonEditor/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace JsonEditor
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged<T>(ref T variable, T value, [CallerMemberName] String propertyName = "")
+        {
+            variable = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        string _JsonString = "{}";
+        public string JsonString { get => _JsonString; set { if (_JsonString != value) NotifyPropertyChanged(ref _JsonString, value); } }
+
+        JObject _Json = JObject.FromObject(new Dictionary<string, object>());
+        public JObject Json { get => _Json; set { if (_Json != value) NotifyPropertyChanged(ref _Json, value); } }
+
+        string _FileName = null;
+        public string FileName { get => _FileName; set { if (_FileName != value) NotifyPropertyChanged(ref _FileName, value); } }
+
+        public bool IsChanged() => Json.ToString() != JsonString;
+    }
+}
